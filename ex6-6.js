f46fef4cd98797e2113b390b23a98fdf0e499b44
hondayama <167833090+hondayama@users.noreply.github.com>
2024-10-22 10:42:35 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,3 +1,3 @@
 var who66 ='world'; function sayhello65(){alert('Hello,' + who65 + '!');}
-function taro66(){alert('太郎が来た);who66 ='太郎';
+function taro66(){who66 ='太郎';alert(who66+'が来た')}
 function hanako66()alert('花子が来た);who66 ='花子';}
