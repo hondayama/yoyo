689fc6bbd47415264ccee798c85e51b5c1492791
hondayama <167833090+hondayama@users.noreply.github.com>
2024-10-29 10:34:03 +0900

    Create ex06-8.js

---
+++ ex06-8.js
@@ -0,0 +1,3 @@
+function sayhello68(){alert('Hello,' + documento.getElementById(‘who68').innerHTML + '!');}
+function taro68(){documento.getElementById(‘who68').innerHTML ='太郎';}
+function hanako68(){documento.getElementById(‘who68').innerHTML ='花子';}
