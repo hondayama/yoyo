a99614ad1c9c6d1c271d1f5fa1716632e0187d94
hondayama <167833090+hondayama@users.noreply.github.com>
2024-11-19 10:37:01 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,4 +1,4 @@
 function imgchange1(){document.getElementById('logo').src ='./image/OIP.jpg';}
 function imgchange2(){document.getElementById('logo').src ='./image/josai_logo.jpg';}
-function imgchange3(){document.getElementById('logo').src ='./image/OIP.jpg';}
-function imgchange4(){document.getElementById('logo').src ='./image/josai_logo.jpg';}
+function imgchange3(){document.getElementById('log').src ='./image/OIP.jpg';}
+function imgchange4(){document.getElementById('log').src ='./image/josai_logo.jpg';}
