2b17df8db19d224176fd629de7c6231656a8fded
hondayama <167833090+hondayama@users.noreply.github.com>
2024-12-03 10:29:12 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,1 +1,2 @@
-function hyoji14(){document.getElementById('lo').src ='./image/OIP.jpg';}
+function hyoji14(){document.getElementById('lo').src ='./image/OIP.jpg'; setTimeaut("hyoji15()",3000);}
+function hyoji15()
