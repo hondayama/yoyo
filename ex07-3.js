bcd1e4a70057b6d35cdca25f78bc7f584523756b
hondayama <167833090+hondayama@users.noreply.github.com>
2024-12-10 10:40:30 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,1 +1,1 @@
-function ad(){var x = document.getElementById('input3').value;var y = document.getElementById('input4').value;alert(x/y);}
+function ad(){var x = document.getElementById('input3').value;var y = document.getElementById('input4').value;alert(x/Math.pow(y,2));}
