b8420a2ae83773ff85b20fc697681dbbfb481d02
hondayama <167833090+hondayama@users.noreply.github.com>
2024-12-17 10:15:03 +0900

    Update ex07-5.js

--- ex07-5.js
+++ ex07-5.js
@@ -1,1 +1,1 @@
-function naz(){ if (document,getElementById('answer').value == '夢'){alert('あたり！');} else{alert('はずれ')}}
+function naz(){ if (document,getElementById('answe').value == '夢'){alert('あたり！');} else{alert('はずれ')}}
