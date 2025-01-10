481b20e7a2f865c3baa1c87e50f1136ed74a014f
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-10 23:33:49 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,2 +1,2 @@
 function add710(){var x = document.getElementById('input7-1').value;var y = document.getElementById('input7-2').value;alert(x/Math.pow(y,2));} if(document.getElementById('x/Math.pow(y,2)').value < '18.5')　{alert('低体重');}　else　if (document.getElementById('x/Math.pow(y,2)').value > '25'){alert('肥満');}　else{alert('普通')}}
-function add710(){if(document.getElementById('input7-1').value == '夢')　{alert('あたり！');}　else　if (document.getElementById('answer79').value == 'ゆめ'){alert('あたり!');}　else{alert('はずれ！')}}
+//function add710(){if(document.getElementById('input7-1').value == '夢')　{alert('あたり！');}　else　if (document.getElementById('answer79').value == 'ゆめ'){alert('あたり!');}　else{alert('はずれ！')}}
