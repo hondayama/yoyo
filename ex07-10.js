7b2e11e628f9948decbe25701a85a633d3947141
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-11 00:05:05 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,2 +1,3 @@
-function add710(){var x = document.getElementById('input7-1').value;var y = document.getElementById('input7-2').value;if(document.getElementById(x/Math.pow(y,2)).value < '18.5') {alert('x/Math.pow(y,2) 低体重');}　else　if (document.getElementById(x/Math.pow(y,2)).value > '25'){alert('x/Math.pow(y,2) 肥満');}　else{alert('x/Math.pow(y,2) 普通')}}
+function add710(){var x = document.getElementById('input7-1').value;var y = document.getElementById('input7-2').value;alert(x/Math.pow(y,2));}
+
 //if(document.getElementById('x/Math.pow(y,2)').value < '18.5')　{alert('低体重');}　else　if (document.getElementById('x/Math.pow(y,2)').value > '25'){alert('肥満');}　else{alert('普通')}}
