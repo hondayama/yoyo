7702aebdf14de73c2df016c271a9fbf55ed4679f
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-10 23:10:20 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,1 +1,1 @@
-function add710(){var x = document.getElementById('input7-1').value;var y = document.getElementById('input7-2').value;alert(Number(x)+Number(y));}
+function add710(){var x = document.getElementById('input7-1').value;var y = document.getElementById('input7-2').value;alert(x/Math.pow(y,2));}
