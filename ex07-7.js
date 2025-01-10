913e9c5d510202db600edf2a61f7582d4727b24b
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-10 22:56:40 +0900

    Update ex07-7.js

--- ex07-7.js
+++ ex07-7.js
@@ -1,1 +1,2 @@
-function nazo77(){if(document.getElementById('answer77').value == '夢')　{alert('あたり！');}　else{alert('はずれ!');}}
+function nazo77(){if(document.getElementById('answer77').value = '夢')　{alert('あたり！');}　else{alert('はずれ!');}}
+function nazo78(){if(document.getElementById('answer78').value == '夢')　{alert('あたり！');}　else{alert('はずれ!');}}
