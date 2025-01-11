1a545ee95fa13ebcc05ed3449c22040aaa3db718
hondayama <167833090+hondayama@users.noreply.github.com>
2025-01-11 15:28:18 +0900

    Update ex-07-11.js

--- ex-07-11.js
+++ ex-07-11.js
@@ -1,1 +1,1 @@
-function uruu711(){var x = document.getElementById('year711').value; if (x % Number(4) == 0){alert('閏年');} else {alert('平年')}}
+function uruu711(){var x = document.getElementById('year711').value; if (Number(x) % Number(4) == 0){alert('閏年');} else {alert('平年')}}
